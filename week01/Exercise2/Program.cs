94ba8d7a3b704ee7891152651ad74b7d54e2eb0f
ghrwn <gaelaherwin@gmail.com>
2025-05-15 11:39:54 -0600

    added grades

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("Enter your grade percentage: ");
+        int gradePercentage = int.Parse(Console.ReadLine());
+
+        string letter = "";
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        string sign = "";
+        int lastDigit = gradePercentage % 10;
+        if (gradePercentage >= 60 && letter != "A" && letter != "F")
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        if (letter == "A" && sign == "+")
+        {
+            sign = "";
+        }
+        if (letter == "F")
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}{sign}.");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Keep trying, you can do better next time!");
+        }
     }
 }
